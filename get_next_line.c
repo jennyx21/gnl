aa660e8d78ac7044e4c0463e340ed8a43a97ed65
jennyx21 <jtruckse@student.42heilbronn.de>
2025-12-04 18:37:46 +0100

    finally the first line printed

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jtruckse <jtruckse@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 15:36:27 by jtruckse          #+#    #+#             */
-/*   Updated: 2025/12/03 23:41:42 by jtruckse         ###   ########.fr       */
+/*   Updated: 2025/12/04 15:31:50 by jtruckse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,9 @@ char	*get_next_line(int fd)
 	char		*line;
 	int			charsread;
 	char		*newlinechar;
-	char		*lineplus;
-	size_t		lengh;
+	char		*lineplus = "";
+	size_t		length1;
+	size_t		length2;
 	static char	buffer[BUFFERSIZE + 1];
 	char		*buf_ptr;
 
@@ -27,15 +28,20 @@ char	*get_next_line(int fd)
 	if (BUFFERSIZE <= 0 || fd < 0)
 		return (NULL);
 	charsread = read(fd, buf_ptr, BUFFERSIZE);
+	length2 = ft_strlen(buf_ptr);
+	line = malloc(length2 + 1);
+	if (!line)
+		return (NULL);
 	line = ft_memcpy(line, buf_ptr, ft_strlennewlinechar(buf_ptr));
 	newlinechar = ft_strchr(line, '\n');
 	if (newlinechar == 0)
 		lineplus = ft_strjoin(lineplus, line);
 	else if (newlinechar > 0)
 	{	
-		lengh = ft_strlennewlinechar(line);
-		lineplus = ft_memcpy(lineplus, line, lengh);
+		length1 = ft_strlennewlinechar(line);
+		lineplus = ft_memcpy(lineplus, line, length1);
 	}
+	free(line);
 	return (lineplus);
 }
 
